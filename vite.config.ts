e5cf06ae2043fd0fffc3c8519cfa3b984e8a307f
KriXPello <krixpello@gmail.com>
2025-12-07 23:17:14 +0300

    configure pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueDevTools from 'vite-plugin-vue-devtools';
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +11,28 @@ export default defineConfig({
     vue(),
     // vueDevTools(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'Журнал',
+        short_name: 'Журнал',
+        description: 'Приложение для учета калорий и ведения заметок',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            "src": "android-chrome-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "android-chrome-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
